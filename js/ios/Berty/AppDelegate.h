d21a4e1223b56e912be554e37dfe40688f5f81ab
Iuri Pereira <iuricmp@gmail.com>
2023-03-07 14:38:42 +0000

    react native update to 0.71.3

--- js/ios/Berty/AppDelegate.h
+++ js/ios/Berty/AppDelegate.h
@@ -1,9 +1,9 @@
+#import <RCTAppDelegate.h>
 #import <React/RCTBridgeDelegate.h>
 #import <UIKit/UIKit.h>
 #import <UserNotifications/UserNotifications.h>
 #import <Expo/Expo.h>
 
 @interface AppDelegate : EXAppDelegateWrapper <UIApplicationDelegate, UNUserNotificationCenterDelegate, RCTBridgeDelegate>
-@property (nonatomic, strong) UIWindow *window;
 
 @end
